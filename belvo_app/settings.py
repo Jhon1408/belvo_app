b3f14a0cf784d5783e24bdc5c89bd5513d1d14b7
jhonatan.ospina <jhonatan.ospina@utp.edu.co>
2023-03-01 23:46:55 -0500

    Fix

--- belvo_app/settings.py
+++ belvo_app/settings.py
@@ -60,6 +60,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "belvo_app.urls"
@@ -153,6 +155,8 @@ CACHEOPS = {
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
