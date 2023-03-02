ebef40d341016b6d5717c2739455ec4063b29ca3
jhonatan.ospina <jhonatan.ospina@utp.edu.co>
2023-03-02 00:45:07 -0500

    Fix

--- belvo_app/settings.py
+++ belvo_app/settings.py
@@ -155,7 +155,6 @@ CACHEOPS = {
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
